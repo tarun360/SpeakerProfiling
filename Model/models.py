232bd1eb2688161ac38132988033de1b1a451edc
tarun360 <tarungupta360@gmail.com>
2021-09-05 16:31:53 +0000

    Added conformer model

--- Model/models.py
+++ Model/models.py
@@ -27,6 +27,31 @@ class UpstreamTransformer(nn.Module):
         age = self.age_regressor(output_averaged)
         gender = self.gender_classifier(output_averaged)
         return height, age, gender
+    
+class UpstreamConformer(nn.Module):
+    def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768):
+        super().__init__()
+        self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        for param in self.upstream.parameters():
+            param.requires_grad = False
+
+        self.conformer_encoder = ConformerEncoder(encoder_dim=feature_dim, num_attention_heads=8, num_layers=num_layers)
+        self.height_regressor = nn.Linear(feature_dim, 1)
+        self.age_regressor = nn.Linear(feature_dim, 1)
+        self.gender_classifier = nn.Sequential(
+            nn.Linear(feature_dim, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = [wav for wav in x.squeeze(1)]
+        x = self.upstream(x)['last_hidden_state']
+        output = self.conformer_encoder(x, x.shape[0])[0]
+        output_averaged = torch.mean(output, dim=1)
+        height = self.height_regressor(output_averaged)
+        age = self.age_regressor(output_averaged)
+        gender = self.gender_classifier(output_averaged)
+        return height, age, gender
 
 # height only models
 
@@ -46,6 +71,24 @@ class UpstreamTransformerH(nn.Module):
         x = [wav for wav in x.squeeze(1)]
         x = self.upstream(x)['last_hidden_state']
         output = self.transformer_encoder(x)
+        output_averaged = torch.mean(output, dim=1)
+        height = self.height_regressor(output_averaged)
+        return height
+
+class UpstreamConformerH(nn.Module):
+    def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768):
+        super().__init__()
+        self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        for param in self.upstream.parameters():
+            param.requires_grad = False
+
+        self.conformer_encoder = ConformerEncoder(encoder_dim=feature_dim, num_attention_heads=8, num_layers=num_layers)
+        self.height_regressor = nn.Linear(feature_dim, 1)
+
+    def forward(self, x):
+        x = [wav for wav in x.squeeze(1)]
+        x = self.upstream(x)['last_hidden_state']
+        output = self.conformer_encoder(x, x.shape[0])[0]
         output_averaged = torch.mean(output, dim=1)
         height = self.height_regressor(output_averaged)
         return height
