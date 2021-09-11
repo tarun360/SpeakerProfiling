b925e703db8f6f7ad2d4dce8091d9956691325c6
tarun360 <tarungupta360@gmail.com>
2021-09-11 15:59:42 +0000

    added option to select number of layers in wav2vec2

--- Model/models.py
+++ Model/models.py
@@ -7,11 +7,15 @@ class UpstreamTransformer(nn.Module):
     def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768, unfreeze_last_conv_layers=False):
         super().__init__()
         self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        
+        # Selecting the 9th encoder layer (out of 12)
+        self.upstream.model.encoder.layers = self.upstream.model.encoder.layers[0:9]
+        
         for param in self.upstream.parameters():
             param.requires_grad = False
         
         if unfreeze_last_conv_layers:
-            for param in self.upstream.model.feature_extractor.conv_layers[6:].parameters():
+            for param in self.upstream.model.feature_extractor.conv_layers[5:].parameters():
                 param.requires_grad = True
         
         encoder_layer = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
@@ -69,11 +73,15 @@ class UpstreamTransformerH(nn.Module):
     def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768, unfreeze_last_conv_layers=False):
         super().__init__()
         self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        
+        # Selecting the 9th encoder layer (out of 12)
+        self.upstream.model.encoder.layers = self.upstream.model.encoder.layers[0:9]
+        
         for param in self.upstream.parameters():
             param.requires_grad = False
         
         if unfreeze_last_conv_layers:
-            for param in self.upstream.model.feature_extractor.conv_layers[6:].parameters():
+            for param in self.upstream.model.feature_extractor.conv_layers[5:].parameters():
                 param.requires_grad = True
         
         encoder_layer = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
