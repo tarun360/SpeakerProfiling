34e762c874b272aeca84d65651715f63605dccc4
Tarun Gupta <tarungupta360@gmail.com>
2021-10-04 19:32:43 +0530

    refactored UncertaintyLoss code

--- Model/utils.py
+++ Model/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from torch.nn import Module, Parameter
-from torch.nn.functional import cross_entropy, interpolate, mse_loss
+from torch.nn.functional import cross_entropy, mse_loss
 
 class RMSELoss(nn.Module):
     def __init__(self):
@@ -19,9 +19,9 @@ class UncertaintyLoss(Module):
         self.loss_height = None
         self.loss_age = None
         self.loss_gender = None
-        self.log_vars_height = Parameter(torch.tensor([0], requires_grad=True, dtype=torch.float32).cuda())
-        self.log_vars_age = Parameter(torch.tensor([0], requires_grad=True, dtype=torch.float32).cuda())
-        self.log_vars_gender = Parameter(torch.tensor([0], requires_grad=True, dtype=torch.float32).cuda())
+        self.log_var_height = Parameter(torch.tensor(0, requires_grad=True, dtype=torch.float32).cuda())
+        self.log_var_age = Parameter(torch.tensor(0, requires_grad=True, dtype=torch.float32).cuda())
+        self.log_var_gender = Parameter(torch.tensor(0, requires_grad=True, dtype=torch.float32).cuda())
 
     def forward(self, input, target):
 
@@ -32,13 +32,13 @@ class UncertaintyLoss(Module):
         height_target, age_target, gender_target = target_arr
 
         self.loss_gender = cross_entropy(input=gender_pred, target=gender_target)
-        self.loss_gender_var = cross_entropy(input=gender_pred*torch.exp(-self.log_vars_gender), target=gender_target)
+        self.loss_gender_var = cross_entropy(input=gender_pred*torch.exp(-self.log_var_gender), target=gender_target)
         
-        self.loss_height = torch.sum((height_target - height_pred) ** 2)
-        self.loss_height_var = torch.exp(-self.log_vars_height) * self.loss_height + self.log_vars_height
+        self.loss_height = mse_loss(input=height_pred, target=height_target)
+        self.loss_height_var = torch.exp(-self.log_var_height) * self.loss_height + self.log_var_height
 
-        self.loss_age = torch.sum((age_target - age_pred) ** 2)
-        self.loss_age_var = torch.exp(-self.log_vars_age) * self.loss_age + self.log_vars_age
+        self.loss_age = mse_loss(input=age_pred, target=age_target)
+        self.loss_age_var = torch.exp(-self.log_var_age) * self.loss_age + self.log_var_age
 
         self.loss = self.loss_gender + self.loss_height + self.loss_age
         
