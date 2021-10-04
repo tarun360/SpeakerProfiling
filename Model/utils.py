d34e3a9c33bad0646f84bae4aad545133b98e2e0
Tarun Gupta <tarungupta360@gmail.com>
2021-10-04 19:33:15 +0530

    refactored UncertaintyLoss code

--- Model/utils.py
+++ Model/utils.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import torch
 import torch.nn as nn
 from torch.nn import Module, Parameter
 from torch.nn.functional import cross_entropy, mse_loss
+
 
 class RMSELoss(nn.Module):
     def __init__(self):
