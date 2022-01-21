42c967fc91428d518b8789ce82b12514c33a9fc2
tarun360 <tarungupta360@gmail.com>
2022-01-21 08:22:23 +0000

    changed config

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class TIMITConfig(object):
     
     ## H
     # wav2vecTransformer
-    model_type = 'UpstreamTransformerAAFC'
+    model_type = 'UpstreamTransformerMoE2'
 
     # RMSE, UncertaintyLoss
     loss = "RMSE"
