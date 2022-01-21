781ddfe046ed2f65a1bc705a1d88ff32f7879e8a
tarun360 <tarungupta360@gmail.com>
2022-01-21 10:22:26 +0000

    added more models

--- Model/models.py
+++ Model/models.py
@@ -178,6 +178,135 @@ class UpstreamTransformerMoE2(nn.Module):
         height = self.height_regressor(output)
         age = self.age_regressor(output)
         return height, age, gender
+
+class UpstreamTransformerMoE3(nn.Module):
+    def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768, unfreeze_last_conv_layers=False):
+        super().__init__()
+        self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        
+        # Selecting the 9th encoder layer (out of 12)
+        self.upstream.model.encoder.layers = self.upstream.model.encoder.layers[0:9]
+        
+        for param in self.upstream.parameters():
+            param.requires_grad = False
+        
+        for param in self.upstream.model.encoder.layers.parameters():
+            param.requires_grad = True
+        
+        encoder_layer = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
+        self.transformer_encoder = torch.nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+        
+        self.fcM = nn.Linear(feature_dim, 1024)
+        self.fcF = nn.Linear(feature_dim, 1024)
+
+        self.height_regressor = nn.Linear(1024, 1)
+        self.age_regressor = nn.Linear(1024, 1)
+        self.gender_classifier = nn.Sequential(
+            nn.Linear(feature_dim, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = [wav for wav in x.squeeze(1)]
+        x = self.upstream(x)['last_hidden_state']
+        x = self.transformer_encoder(x)
+        x = torch.mean(x, dim=1)
+        gender = self.gender_classifier(x)
+        xM = self.fcM(x)
+        xF = self.fcF(x)
+        output = (1-gender)*xM + gender*xF
+        height = self.height_regressor(output)
+        age = self.age_regressor(output)
+        return height, age, gender
+    
+class UpstreamTransformerMoE4(nn.Module):
+    def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768, unfreeze_last_conv_layers=False):
+        super().__init__()
+        self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        
+        # Selecting the 9th encoder layer (out of 12)
+        self.upstream.model.encoder.layers = self.upstream.model.encoder.layers[0:9]
+        
+        for param in self.upstream.parameters():
+            param.requires_grad = False
+        
+        for param in self.upstream.model.encoder.layers.parameters():
+            param.requires_grad = True
+        
+        encoder_layer = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
+        self.transformer_encoder = torch.nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+        
+        self.fcM = nn.Linear(2*feature_dim, 1024)
+        self.fcF = nn.Linear(2*feature_dim, 1024)
+        
+        self.dropout = nn.Dropout(0.5)
+
+        self.height_regressor = nn.Linear(1024, 1)
+        self.age_regressor = nn.Linear(1024, 1)
+        self.gender_classifier = nn.Sequential(
+            nn.Linear(2*feature_dim, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = [wav for wav in x.squeeze(1)]
+        x = self.upstream(x)['last_hidden_state']
+        x = self.transformer_encoder(x)
+        x = torch.cat((torch.mean(x, dim=1), torch.std(x, dim=1)), dim=1)
+        gender = self.gender_classifier(x)
+        xM = self.dropout(self.fcM(x))
+        xF = self.dropout(self.fcF(x))
+        output = (1-gender)*xM + gender*xF
+        height = self.height_regressor(output)
+        age = self.age_regressor(output)
+        return height, age, gender
+    
+class UpstreamTransformerMoE5(nn.Module):
+    def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768, unfreeze_last_conv_layers=False):
+        super().__init__()
+        self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
+        
+        # Selecting the 9th encoder layer (out of 12)
+        self.upstream.model.encoder.layers = self.upstream.model.encoder.layers[0:9]
+        
+        for param in self.upstream.parameters():
+            param.requires_grad = False
+        
+        for param in self.upstream.model.encoder.layers.parameters():
+            param.requires_grad = True
+        
+        encoder_layer_M = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
+        self.transformer_encoder_M = torch.nn.TransformerEncoder(encoder_layer_M, num_layers=num_layers)
+        
+        encoder_layer_F = torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True)
+        self.transformer_encoder_F = torch.nn.TransformerEncoder(encoder_layer_F, num_layers=num_layers)
+        
+        self.fcM = nn.Linear(2*feature_dim, 1024)
+        self.fcF = nn.Linear(2*feature_dim, 1024)
+        
+        self.dropout = nn.Dropout(0.5)
+
+        self.height_regressor = nn.Linear(1024, 1)
+        self.age_regressor = nn.Linear(1024, 1)
+        self.gender_classifier = nn.Sequential(
+            nn.Linear(4*feature_dim, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = [wav for wav in x.squeeze(1)]
+        x = self.upstream(x)['last_hidden_state']
+        xM = self.transformer_encoder_M(x)
+        xF = self.transformer_encoder_F(x)
+        xM = torch.cat((torch.mean(xM, dim=1), torch.std(xM, dim=1)), dim=1)
+        xF = torch.cat((torch.mean(xF, dim=1), torch.std(xF, dim=1)), dim=1)
+        xM = self.dropout(self.fcM(x))
+        xF = self.dropout(self.fcF(x))
+        gender = self.gender_classifier(torch.cat((xM, xF)))
+        output = (1-gender)*xM + gender*xF
+        height = self.height_regressor(output)
+        age = self.age_regressor(output)
+        return height, age, gender
     
 class UpstreamTransformerAAFC(nn.Module):
     def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768, unfreeze_last_conv_layers=False):
--- TIMIT/lightning_model.py
+++ TIMIT/lightning_model.py
@@ -13,7 +13,7 @@ import pandas as pd
 import torch_optimizer as optim
 
 
-from Model.models import UpstreamTransformer, UpstreamTransformerFC, UpstreamTransformerAAFC, UpstreamTransformer3SeparateFC, UpstreamTransformerMoE, UpstreamTransformerMoE2
+from Model.models import UpstreamTransformer, UpstreamTransformerFC, UpstreamTransformerAAFC, UpstreamTransformer3SeparateFC, UpstreamTransformerMoE, UpstreamTransformerMoE2, UpstreamTransformerMoE3, UpstreamTransformerMoE4
 
 class RMSELoss(nn.Module):
     def __init__(self):
@@ -34,7 +34,9 @@ class LightningModel(pl.LightningModule):
             'UpstreamTransformerAAFC': UpstreamTransformerAAFC,
             'UpstreamTransformer3SeparateFC': UpstreamTransformer3SeparateFC,
             'UpstreamTransformerMoE': UpstreamTransformerMoE,
-            'UpstreamTransformerMoE2': UpstreamTransformerMoE2
+            'UpstreamTransformerMoE2': UpstreamTransformerMoE2,
+            'UpstreamTransformerMoE3': UpstreamTransformerMoE3,
+            'UpstreamTransformerMoE4': UpstreamTransformerMoE4
         }
         
         self.model = self.models[HPARAMS['model_type']](upstream_model=HPARAMS['upstream_model'], num_layers=HPARAMS['num_layers'], feature_dim=HPARAMS['feature_dim'], unfreeze_last_conv_layers=HPARAMS['unfreeze_last_conv_layers'])
