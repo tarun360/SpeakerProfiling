7ec0d46cbbc4c11cf93ea46fb8f8d32b16f6d574
ductuantruong <ductuanpy99@gmail.com>
2022-02-23 02:10:09 +0000

    stop feed mfcc to transformer encoder

--- Model/models.py
+++ Model/models.py
@@ -7,7 +7,7 @@ class UpstreamTransformer(nn.Module):
     def __init__(self, upstream_model='wav2vec2',num_layers=6, feature_dim=768, unfreeze_last_conv_layers=False):
         super().__init__()
         self.upstream = torch.hub.load('s3prl/s3prl', upstream_model)
-        self.mfcc_feature_dim = 32
+        self.mfcc_feature_dim = 128
         # Selecting the 9th encoder layer (out of 12)
         self.upstream.model.encoder.layers = self.upstream.model.encoder.layers[0:9]
         
@@ -18,22 +18,23 @@ class UpstreamTransformer(nn.Module):
             for param in self.upstream.model.feature_extractor.conv_layers[5:].parameters():
                 param.requires_grad = True
         
-        self.transformer_encoder_1 = torch.nn.TransformerEncoder(torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=6, batch_first=True), num_layers=num_layers)
-        self.transformer_encoder_2 = torch.nn.TransformerEncoder(torch.nn.TransformerEncoderLayer(d_model=self.mfcc_feature_dim, nhead=4, batch_first=True), num_layers=num_layers)
+        self.transformer_encoder_1 = torch.nn.TransformerEncoder(torch.nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8, batch_first=True), num_layers=num_layers)
+        #self.transformer_encoder_2 = torch.nn.TransformerEncoder(torch.nn.TransformerEncoderLayer(d_model=self.mfcc_feature_dim, nhead=8, batch_first=True), num_layers=num_layers)
         
-        self.final_feature_dim = 800
+        self.final_feature_dim = 896
         
         self.height_regressor = nn.Sequential(
-            nn.Linear(self.final_feature_dim, 128),
-            nn.Linear(128, 1),
+            nn.Linear(self.final_feature_dim, 256),
+            nn.GELU(),
+            nn.Linear(256, 1),
         )
         self.age_regressor = nn.Sequential(
-            nn.Linear(self.final_feature_dim, 128),
-            nn.Linear(128, 1),
+            nn.Linear(self.final_feature_dim, 256),
+            nn.GELU(),
+            nn.Linear(256, 1),
         )
         self.gender_classifier = nn.Sequential(
-            nn.Linear(self.final_feature_dim, 128),
-            nn.Linear(128, 1),
+            nn.Linear(self.final_feature_dim, 1),
             nn.Sigmoid()
         )
 
@@ -41,7 +42,8 @@ class UpstreamTransformer(nn.Module):
         x = [wav for (i,wav) in enumerate(x.squeeze(1))]
         x = self.upstream(x)['last_hidden_state']
         output_1 = self.transformer_encoder_1(x)
-        output_2 = self.transformer_encoder_2(mfcc_x.squeeze(1))
+        #output_2 = self.transformer_encoder_2(mfcc_x.squeeze(1).cuda())
+        output_2 = mfcc_x.squeeze(1).cuda()
         output_averaged_1 = torch.mean(output_1, dim=1)
         output_averaged_2 = torch.mean(output_2, dim=1)
         concat_output = torch.cat((output_averaged_1, output_averaged_2), dim=-1)
